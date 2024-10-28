2476d4c04c60b53dff775246b54aa468481fb339
abdwalid04 <adil.walid.abdellah@gmail.com>
2024-10-29 00:58:47 +0100

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import gradio as gr
 
 from background_replacer import replace_background
 
-developer_mode = os.getenv('DEV_MODE', False)
+developer_mode = os.getenv('DEV_MODE', True)
 
 DEFAULT_POSITIVE_PROMPT = "on the pavement, poolside, idyllic infinity pool, Hawaiian hilltops, commercial product photography"
 DEFAULT_NEGATIVE_PROMPT = ""
@@ -33,31 +33,14 @@ EXAMPLES = [
 ]
 
 INTRO = """
-# Shopify Image Background Replacement
-
-[![Duplicate this Space](https://huggingface.co/datasets/huggingface/badges/resolve/main/duplicate-this-space-md.svg)](https://huggingface.co/spaces/Shopify/background-replacement?duplicate=true)
-Minimum recommended hardware: Nvidia A10G large (46 GB RAM, 24 GB VRAM)
-
-## Status
-🏝️ Since the publication of this prototype, we've devoted our efforts to developing an enhanced version within Shopify's admin interface, which is now accessible to all Shopify merchants across all subscription plans. This original space is no longer maintained and runs on a CPU-only free tier. Please duplicate this space and utilize your own GPUs.
+# Image Background Replacement
 
 <hr>
 
-Building an online store requires lots of high quality product and marketing images. This is an early demo of a background replacement tool built with Stable Diffusion XL that makes it easy to use your existing product images to make something new. Please be patient during peak demand. 😅
-
-To use it, upload your product photo (.jpg or .png), then describe the background you’d like to see in place of the original. For best results follow the general pattern in the examples below:
-1. ❌ _Do not_ describe your product in the prompt (ex: black sneakers)
-2. ✅ Do describe the "grounding" for your product (ex: placed on a table)
-3. ✅ Do describe the scene you want (ex: in a greek cottage)
-4. ✅ Do describe a style of image (ex: side view commercial product photography)
-5. 🤔 Optionally, describe what you want to avoid 🙅 in the negative prompt field
 """
 
 MORE_INFO = """
-### More information
-- You can check our [FAQs here](https://huggingface.co/spaces/Shopify/background-replacement/blob/main/README.md#faqs)!
-- We are also gathering resources from the community and sharing ideas [here](https://huggingface.co/spaces/Shopify/background-replacement/discussions).
-- Shopify is on a mission to redefine commerce with AI. If you’re an AI or ML engineer looking to build the future of commerce, [join us](https://www.shopify.com/careers)!
+
 """
 
 
@@ -181,7 +164,7 @@ with gr.Blocks(css=custom_css) as iface:
 
     # Enable this button!
     gen_button = gr.Button(
-        value="Generate!", variant="primary", interactive=False)
+        value="Generate!", variant="primary", interactive=True)
 
     with gr.Tab('Results'):
         results = gr.Gallery(
